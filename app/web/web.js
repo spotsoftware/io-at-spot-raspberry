d3efa5e84877af7aaddbbb5c9eb963c62fc25851
andrea-spotsoftware <andrea.corzani@spot.it>
2019-01-14 09:44:37 +0100

    CORS

--- app/web/web.js
+++ app/web/web.js
@@ -9,6 +9,13 @@ var express = require('express'),
 var app = express(),
     _listener;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));                        //middleware to parse url
